10284d7034a508b5924b40c53daa0272bcc58d3f
Nishi Kanta Paul <nishi.kanta@brainstation-23.com>
2025-04-30 19:12:59 +0600

    feat: remove unused React import in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
